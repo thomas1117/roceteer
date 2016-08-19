3117f6abd5de551560ca58322fc51f1ac535e79f
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-08-18 21:05:20 -0700

    added query param grab value service

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,19 @@ $(document).ready(function(){
 
 })
 
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]).replace(/\+/g," ");
+        }
+    }
+    
+    return undefined;
+}
+
 function toggleActive(num) {
 		
 		if(num==1) {
@@ -18,5 +31,26 @@ function toggleActive(num) {
 			$("#content-3").show();
 			$("#content-1").hide();
 			$("#content-2").hide();
+		}
+}
+
+function handleService() {
+		
+		var id = getQueryVariable('id');
+
+		if(id==1) {
+			$("#service-content-1").show();
+			$("#service-content-2").hide();
+			$("#service-content-3").hide();
+		}
+		else if(id==2) {
+			$("#service-content-2").show();
+			$("#service-content-1").hide();
+			$("#service-content-3").hide();
+		}
+		else {
+			$("#service-content-3").show();
+			$("#service-content-1").hide();
+			$("#service-content-2").hide();
 		}
 }
